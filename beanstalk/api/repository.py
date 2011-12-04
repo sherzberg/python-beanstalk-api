8f9f0786ba5254873d85de7a83ae014dcf50210e
Spencer Herzberg <spencer.herzberg@gmail.com>
2011-12-04 17:49:38 -0600

    implemented repository.create

--- beanstalk/api/repository.py
+++ beanstalk/api/repository.py
@@ -10,3 +10,16 @@ class Repository(Base):
             url = 'repositories.json'
             
         return self._do_request(url)
+    
+    def create(self, name, title, color_label='label-white', vcs='subversion',  create_structure=False):
+        url = 'repositories.json'
+        data = {
+                'repository': {
+                               'name': name,
+                               'title': title,
+                               'type_id': vcs,
+                               'color_label': color_label
+                               }
+                }
+
+        return self._do_request(url, data)
