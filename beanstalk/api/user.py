7021aba071a3015165b5624f96a5f25727c4a613
Spencer Herzberg <spencer.herzberg@gmail.com>
2011-12-10 13:12:24 -0600

    implmented user create and update

--- beanstalk/api/user.py
+++ beanstalk/api/user.py
@@ -10,3 +10,40 @@ class User(Base):
             url = 'users.json'
             
         return self._do_request(url)
+    
+    def create(self, login, first_name, last_name, email, password, admin=False):
+        url = 'users.json'
+        
+        data = {
+                'user': {
+                         'login': login,
+                         'first_name': first_name,
+                         'last_name': last_name,
+                         'email': email,
+                         'password': password
+                         }
+                }
+        if admin:
+            data['user']['admin'] = True
+            
+        return self._do_request(url, data)
+    
+    def update(self, id, first_name=None, last_name=None, email=None, password=None, admin=None):
+        url = 'users/{0}.json'.format(id)
+        data ={'user': {}}
+        
+        if first_name:
+            data['user']['first_name'] = first_name
+        if last_name:
+            data['user']['last_name'] = last_name
+        if email:
+            data['user']['email'] = email
+        if password:
+            data['user']['password'] = password
+        if admin != None:
+            data['user']['admin'] = admin
+        
+        print data
+        
+        return self._do_request(url, data)
+    
