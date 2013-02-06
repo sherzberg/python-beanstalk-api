32d3183a0819ab292adbb1ebeedb2e7fe76e4a80
Khoi <tranlongkhoi@gmail.com>
2013-02-06 11:54:35 -0800

    removed ServerEnvironment, using Environment instead

--- beanstalk/api/__init__.py
+++ beanstalk/api/__init__.py
@@ -7,7 +7,6 @@ from .permission import Permission
 from .environment import Environment
 from .changeset import Changeset
 from .release import Release
-from .server_environment import ServerEnvironment
 
 user = User()
 repository = Repository()
@@ -17,4 +16,3 @@ permission = Permission()
 environment = Environment()
 changeset = Changeset()
 release = Release()
-server_environment = ServerEnvironment()
