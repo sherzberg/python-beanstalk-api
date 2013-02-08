067d6d8a463ad062a9411c19cf5786be7dcca8c0
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-02-07 20:59:47 -0600

    adding docs

--- beanstalk/api/invitation.py
+++ beanstalk/api/invitation.py
@@ -2,32 +2,60 @@ from .base import Base
 
 
 class Invitation(Base):
+    """This class manages and creates invitations to the Beanstalk service
+
+    Readable Attribtues:
+        id           : (integer) Unique ID of the Invitation.
+        account_id   : (integer) ID of the Account Invitation belongs to.
+        creator_id   : (integer) ID of the User that created the Invitation.
+        creator_name : (string) Name of the User that created the Invitation.
+        creator_email: (string) Email address of the User that created the Invitation.
+        secure_token : (string) Unique secure key that is used by a signup_url.
+        signup_url   : (string) URL that can be used to finalize the invitation.
+        updated_at   : (datetime) Time when the Invitation was last updated.
+        created_at   : (datetime) Time when the Invitation was first added to the system.
+
+    Writable Attributes:
+        email        : (string) Required on create. Must be unique for Account.
+        name         : (string) Full name. Required on create.
+    """
 
     def find(self, invitation_id):
+        """Find an invitation by invitation id"""
+
         url = 'invitations/{0}.json'.format(invitation_id)
 
         return self._do_get(url)
 
     def find_all(self):
+        """Find all invitations"""
+
         url = 'invitations.json'
 
         return self._do_get(url)
 
-    def create(self, email, first_name, last_name):
+    def create(self, email, name):
+        """This method will create both User and Invitation. A usual
+        invitation email with signup link will be delivered to the User
+        """
+
         url = 'invitations.json'
         data = {
             "invitation": {
                 "user": {
                     "email": email,
-                    "first_name": first_name,
-                    "last_name": last_name
+                    "name": name
                 }
             }
         }
 
         return self._do_post(url, data)
 
-    def resend(self, user_id):
-        url = 'invitations/resend/{0}.json'.format(user_id)
+    def resend(self, email):
+        """This method will resend an invitation to the user's email
+        address
+        """
+
+        url = 'invitations/resend/{0}.json'.format(email)
 
         return self._do_get(url)
--- beanstalk/api/user.py
+++ beanstalk/api/user.py
@@ -2,8 +2,33 @@ from .base import Base
 
 
 class User(Base):
+    """This class manages and creates Users
+
+    Readable Attributes:
+        id        : (integer) Unique ID of the User.
+        account_id: (integer) ID of teh Account user belongs to.
+        login     : (string) Username. Unique per Account.
+        email     : (string) Email Address. Unique per Account.
+        name      : (string) Full Name.
+        first_name: (string) First Name.
+        last_name : (string) Last Name.
+        owner     : (boolean) True if User has created the Account initially.
+        admin     : (boolean) True if user has admin privileges in the Account.
+        timezone  : (string) User's preferred time zone.
+        updated_at: (datetime) Time when the User was last updated.
+        created_at: (datetime) Time when the User was first added to the system.
+
+    Writable Attributes:
+        login     : (string) Writable only on create. Always required and must be unique.
+        email     : (string) Required on create.
+        name      : (string) Required on create.
+        password  : (string) Required on create.
+        admin     : (boolean) Optional.
+        timezone  : (string) Optional.
+        """
 
     def find(self, user_id=None):
+        """This method finds a specific user by ID"""
         if user_id:
             url = 'users/{0}.json'.format(user_id)
         else:
@@ -11,24 +36,43 @@ class User(Base):
 
         return self._do_get(url)
 
-    def create(self, login, first_name, last_name, email, password, admin=False):
+    def find_all(self, page=None, per_page=30):
+        """This method finds all users. This method supports pagination that is disabled
+        by default.
+        """
+
+        url = 'users.json'
+
+        if page:
+            url += '?page={0}&per_page?{1}'.format(page, per_page)
+
+        return self._do_get(url)
+
+    def create(self, login, name, email, password, admin=False, timezone=None):
+        """This method will create a User"""
+
         url = 'users.json'
 
         data = {
             'user': {
                 'login': login,
-                'first_name': first_name,
-                'last_name': last_name,
+                'name': name,
                 'email': email,
                 'password': password
             }
         }
+
         if admin:
             data['user']['admin'] = True
+
+        if timezone:
+            data['user']['timezone'] = timezone
 
         return self._do_post(url, data)
 
     def update(self, id, first_name=None, last_name=None, email=None, password=None, admin=None):
+        """This method will update a User"""
+
         url = 'users/{0}.json'.format(id)
         data = {
             'user': {}
@@ -52,3 +96,10 @@ class User(Base):
         print(data)
 
         return self._do_put(url, data=data)
+
+    def delete(self, id):
+        """This method will delete a User"""
+
+        url = 'users/{0}.json'.format(id)
+
+        return self._do_delete(url)
